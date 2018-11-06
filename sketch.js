7dbab113484cc4c53d74ca49214aacdfcebfa157
Matteo-Tarda <t_matteo@ymail.com>
2018-11-06 15:23:23 +0100

    Finished assignment.

--- sketch.js
+++ sketch.js
@@ -1,11 +1,40 @@
-function preload(){
-  // put preload code here
+'use strict'
+var preload = () => { // eslint-disable-line
+    // put preload code here
 }
 
-function setup() {
-  // put setup code here
+let capture;
+
+var setup = () => { // eslint-disable-line
+    background(255);
+
+    createCanvas(windowWidth, windowHeight);
+    ellipseMode(CENTER);
+
+    capture = createCapture(VIDEO);
+    capture.size(640, 480);
+    capture.hide();
+    noStroke();
+
 }
 
-function draw() {
-  // put drawing code here
+
+var draw = () => { // eslint-disable-line
+    background(0);
+    let img = capture.loadPixels();
+    let hi = width/640;
+    let vi = height/480;
+    let s;
+    for (let x = 0; x < 640; x+=10) {
+        for (let y = 0; y < 480; y+=20) {
+            fill(img.get(x, y))
+            s = 20/(dist(x*hi,y*vi,mouseX,mouseY)/100);
+            if (s>40) s=40;
+            if((x+y)%20){
+                triangle(x*hi - s, y*vi + s, x*hi, y*vi - s, x*hi + s, y*vi + s);
+            }else{
+                triangle(x*hi - s, y*vi - s, x*hi, y*vi + s, x*hi + s, y*vi - s);
+            }
+        }
+    }
 }
